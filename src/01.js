9f495ac43490e8ea910ada0016174be99cb38c5d
rerades <rerades@siete3.com>
2021-07-06 06:22:56 +0200

    movePArticle

---
+++ src/01.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("canvas");  // obtenemos el elemento del canvas
+const ctx = canvas.getContext("2d"); // obtenemos el contexto 2d del canvas
+const particleList = [];
+
+const loop = (idx,canvas,particleList) => {
+  requestAnimationFrame(
+    () => {
+      canvas.width = document.body.clientWidth;
+      canvas.height = document.body.clientHeight;
+      console.log(idx)
+      idx && loop(--idx,canvas,particleList);
+    }
+  )
+}
+
+loop(10,canvas, particleList);
+
--- src/index.js
+++ src/index.js
@@ -1,51 +1,29 @@
-const cleanup = (canvas) => {
-  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+const canvas = document.getElementById("canvas");  // obtenemos el elemento del canvas
+const ctx = canvas.getContext("2d"); // obtenemos el contexto 2d del canvas
+
+
+const Particle = {
+  position: {x:0, y:0},
+  velocity: {x:1, y:1},
+  color: "FA12342A",
+  size: 5
 };
 
-const circleShape = canvas => particle => {
-  //console.log(Math.random());
+const circleShape = (canvas,particle) => {
   const context = canvas.getContext("2d");
   return {
     run: () => {
       context.fillStyle = particle.color;
       context.beginPath();
-      context.arc(particle.position.x, particle.position.y, particle.size, 0, 2 * Math.PI, false);
+      context.arc(particle.position.x, particle.position.y, particle.size, 0, 2 * Math.PI);
       context.fill();
     }
-  };
-};
-/// ---------------
+  }
+}
 
-const ParticleA = {
-  position: { x: 300, y: 100 },
-  velocity: { x: 1, y: 1 },
-  acceleration: { x: 1, y: 1 },
-  color: "rgba(243,194,6,0.91)",
-  size: 5,
-};
-
-const ParticleB = {
-  position: { x: 200, y: 300 },
-  velocity: { x: -2, y: -5 },
-  acceleration: { x: 0, y: 0 },
-  color: "rgba(23,241,241,0.61)",
-  size: 5,
-};
-
-const ParticleC = {
-  position: { x: 400, y: 400 },
-  velocity: { x: 1, y: 1 },
-  acceleration: { x: 1, y: -2 },
-  color: "rgba(158,232,18,0.68)",
-  size: 5,
-};
-
-const particles = [
-  ParticleA,
-  ParticleB,
-  ParticleC,
-]
-
+const cleanup = (canvas) => {
+  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+}
 
 const randomNum = (num) => Math.round(Math.random() * num);
 
@@ -54,66 +32,40 @@ const randomParticle = () => {
     position: { x: randomNum(500) , y: randomNum(500) },
     velocity: { x: randomNum(10) - 5, y: randomNum(10) - 5 },
     acceleration: { x: randomNum(2) - 1, y: randomNum(2) - 1 },
-    color: `rgba(${randomNum(255)},${randomNum(255)},${randomNum(255)},${Math.random()})`,
+    color: `rgba(${randomNum(255)},${randomNum(255)},${randomNum(255)}, ${Math.random()})`,
     size: randomNum(10),
   }
 }
 
-const vectorAdd = (p1, p2) => ({x : p1.x + p2.x ,y : p1.y+ p2.y});
+const particleList = [
+  randomParticle(),
+  randomParticle(),
+  randomParticle()
+];
 
 // moveParticle :: particle -> particle
 const moveParticle = (particle) => {
-  particle.position = vectorAdd(particle.position, particle.velocity )
+  particle.position.x = particle.position.x + particle.velocity.x;
+  particle.position.y = particle.position.y + particle.velocity.y;
   return particle
 }
 
-const add = (n,inc) => n + inc;
-const changeParticleSize = (particle) => {
-  const inc = particle.size === 50 ? 0 : 1;
-  particle.size = add(particle.size,inc);
-  return particle;
+
+
+const loop = (idx,canvas,particleList) => {
+  requestAnimationFrame(
+    () => {
+      canvas.width = document.body.clientWidth;
+      canvas.height = document.body.clientHeight;
+      particleList.forEach(
+        (particle) => {
+          circleShape(canvas, moveParticle(particle)).run()
+        }
+      )
+      idx && loop(--idx,canvas,particleList);
+    }
+  )
 }
 
-const compose = (...functions) => arg =>
-  functions.reduceRight((result, fn) => fn(result), arg);
-// map:: a -> xs -> xs
-const map = fn => xs => xs.map(fn);
-
-
-
-
-function start(canvas,idx,particleList) {
-  // OJO::: NO METER ESTo AQUI !!!!
-  // canvas.width = document.body.clientWidth;
-  // canvas.height = document.body.clientHeight;
-  const createShapeInCanvas = circleShape(canvas);
-
-  requestAnimationFrame(function draw() {
-    // console.log(canvas.width , canvas.height);
-    // para mantener siempre el canvas al tamaño del documento
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-
-    // cleanup
-    cleanup(canvas);
-    // add particle;
-    particleList.push(randomParticle());
-
-    particleList.forEach(
-      (particle) => {
-        compose(
-          createShapeInCanvas,
-          changeParticleSize,
-          moveParticle
-        )(particle).run();
-      }
-      );
-
-     start(canvas, idx, particleList);
-    });
-}
-
-
-const canvasEl = document.getElementById('canvas');
-start(canvasEl,  1, particles);
+loop(1000,canvas, particleList);
 
